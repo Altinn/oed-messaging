6a8e155cee702f64b4a536a137f99367e64cbf8f
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2026-05-11 12:35:38 +0200

    Added the CaseStatusManuallySynced event type

--- Altinn.Dd.InternalEvents/EventType.cs
+++ Altinn.Dd.InternalEvents/EventType.cs
@@ -5,8 +5,20 @@
 /// </summary>
 public static class EventType
 {
-    //public const string CaseStatusUpdated = "no.altinn.events.digitalt-dodsbo.v1.case-status-updated";
+    /// <summary>
+    /// Event published by oed-events when ever a new update is received from DA via hendelsesliste
+    /// </summary>
     public const string CaseStatusUpdateValidated = "no.altinn.events.digitalt-dodsbo.v1.case-status-update-validated";
+    /// <summary>
+    /// Event published by oed-events when a single case object is fetched and synced based on a manual trigger.
+    /// </summary>
+    public const string CaseStatusManuallySynced = "no.altinn.events.digitalt-dodsbo.v1.case-status-manually-synced";
+    /// <summary>
+    /// Event published by oed when a declaration is submitted for an estate by one of the heirs
+    /// </summary>
     public const string DeclarationSubmitted = "no.altinn.events.digitalt-dodsbo.v1.declaration-submitted";
+    /// <summary>
+    /// Event published by oed-declaration when a claim has been changed requiring the other heirs to sign the declaration again
+    /// </summary>
     public const string DeclarationUnsigned = "no.altinn.events.digitalt-dodsbo.v1.declaration-unsigned";
 }
