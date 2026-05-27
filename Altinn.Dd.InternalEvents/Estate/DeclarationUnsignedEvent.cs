37e0d29be4d7100456b3d98839ddf30750bc9f28
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2026-05-27 13:22:49 +0200

    Fix missing property getter and setters for SigneePartyIds in DeclarationUnsignedEvent

--- Altinn.Dd.InternalEvents/Estate/DeclarationUnsignedEvent.cs
+++ Altinn.Dd.InternalEvents/Estate/DeclarationUnsignedEvent.cs
@@ -5,5 +5,5 @@ namespace Altinn.Dd.InternalEvents.Estate;
 public class DeclarationUnsignedEvent
 {
     [JsonPropertyName("signeePartyIds")]
-    public required IEnumerable<string> SigneePartyIds = [];
+    public required IEnumerable<string> SigneePartyIds { get; set; }
 }
