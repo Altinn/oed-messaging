9333e85d091da433979d7d8e0c9e728e544134f5
espen.lonnkvist <espen.smith.lonnkvist@digdir.no>
2026-06-25 14:22:29 +0200

    Add resource id constants

---
+++ Altinn.Dd.InternalEvents/ResourceId.cs
@@ -0,0 +1,11 @@
+﻿namespace Altinn.Dd.InternalEvents;
+
+/// <summary>
+/// A list of the resource IDs for the internal events that are being published in the Digitalt Dødsbo system
+/// </summary>
+public static class ResourceId
+{
+    public const string DdPrivateProbate = "urn:altinn:resource:app_digdir_dd-private-probate";
+    public const string OedDeclaration = "urn:altinn:resource:app_digdir_oed-declaration";
+    public const string DomstolApi = "urn:altinn:resource:dodsbo-domstoladmin-api";
+}
