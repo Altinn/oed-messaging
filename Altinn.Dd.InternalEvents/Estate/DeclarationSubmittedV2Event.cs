cbf01decfb37d9c26e1c017691c9dfd0ce725fa2
espen.lonnkvist <espen.smith.lonnkvist@digdir.no>
2026-06-29 12:58:02 +0200

    Add DeclarationSubmittedV2Event

---
+++ Altinn.Dd.InternalEvents/Estate/DeclarationSubmittedV2Event.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.Dd.InternalEvents.Estate;
+
+public class DeclarationSubmittedV2Event
+{
+    [JsonPropertyName("daCaseId")]
+    public required string DaCaseId { get; set; }
+}
